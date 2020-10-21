d872ab5bbe578c0bc0fa48cff3c38b0d7c5eff13
Christopher Henderson <chenderson5280@gmail.com>
2020-10-21 17:10:20 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -46,18 +46,18 @@ def game_hash
                   :steals => 4,
                   :blocks => 11,
                   :slam_dunks => 1}]
-        },
+        }
   :away => {:team_name => "Charlotte Hornets",
            :colors => ["Turquoise", "Purple"],
            :players => [{:player_name => "Jeff Adrien",
-                       :number => "0",
-                       :shoe => "16",
-                       :points => "22",
-                       :rebounds => "12",
-                       :assists => "12",
-                       :steals => "3",
-                       :blocks => "1",
-                       :slam_dunks => "1"},{},{},{},{}]}
+                       :number => 4,
+                       :shoe => 18,
+                       :points => 10,
+                       :rebounds => 1,
+                       :assists => 1,
+                       :steals => 2,
+                       :blocks => 7,
+                       :slam_dunks => 2},{},{},{},{}]}
 }
 
 
