8594f06f3cdd095b72e6aba422892a65b3c669d1
Christopher Henderson <chenderson5280@gmail.com>
2020-10-21 16:56:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -47,9 +47,17 @@ def game_hash
                   :blocks => "11",
                   :slam_dunks => "1"}]
         },
-  :away => {:team_name => "Brooklyn Nets",
-           :colors => ["Black","White"],
-           :players => []}
+  :away => {:team_name => "Charlotte Hornets",
+           :colors => ["Turquoise", "Purple"],
+           :players => [{:player_name => "Alan Anderson",
+                       :number => "0",
+                       :shoe => "16",
+                       :points => "22",
+                       :rebounds => "12",
+                       :assists => "12",
+                       :steals => "3",
+                       :blocks => "1",
+                       :slam_dunks => "1"},{},{},{},{}]}
 }
 
 
