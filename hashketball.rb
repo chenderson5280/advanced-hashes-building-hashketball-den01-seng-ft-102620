25cf1c530ed283889b52c20049e72cf36282e430
Christopher Henderson <chenderson5280@gmail.com>
2020-10-21 16:53:32 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -47,7 +47,6 @@ def game_hash
                   :blocks => "11",
                   :slam_dunks => "1"}]
         },
-
   :away => {:team_name => "Brooklyn Nets",
            :colors => ["Black","White"],
            :players => []}
