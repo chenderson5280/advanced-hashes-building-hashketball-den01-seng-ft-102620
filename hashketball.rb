d5351c8c8e165269db9a68a0a6254a16073ce193
Christopher Henderson <chenderson5280@gmail.com>
2020-10-21 16:14:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,7 @@
 def game_hash
 {:home => {:team_name => "",
-          :colors => [],
-          :player => [{},{}]
+          :colors => ["",""],
+          :players => [{},{}]
         }
 
   :away => {}
@@ -9,3 +9,17 @@ def game_hash
 
 
 end
+
+The :players key points to an Array of Hashes. Each Hash in the Array should contain 
+the players' stats. Each stat should be a key that points 
+to a value for that stat. The keys should be:'
+
+:player_name
+:number
+:shoe
+:points
+:rebounds
+:assists
+:steals
+:blocks
+:slam_dunks
