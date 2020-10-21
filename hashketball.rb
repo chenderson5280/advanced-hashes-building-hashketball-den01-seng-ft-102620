a1e9efbf8f9ebc292a2d04d44acdf87dbf9e07b5
Christopher Henderson <chenderson5280@gmail.com>
2020-10-21 17:16:11 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -71,11 +71,11 @@ def game_hash
                   :number => 2,
                   :shoe => 14,
                   :points => 24,
-                  :rebounds => 1,
-                  :assists => 1,
-                  :steals => 2,
-                  :blocks => 7,
-                  :slam_dunks => 2},{},{}]}
+                  :rebounds => 12,
+                  :assists => 12,
+                  :steals => 4,
+                  :blocks => 5,
+                  :slam_dunks => 5},{},{}]}
 }
 
 
