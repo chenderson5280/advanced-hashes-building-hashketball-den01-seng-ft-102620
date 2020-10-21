7e9ee7a219161c84aab34b2d3e4f537be9788779
Christopher Henderson <chenderson5280@gmail.com>
2020-10-21 17:13:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -57,7 +57,16 @@ def game_hash
                        :assists => 1,
                        :steals => 2,
                        :blocks => 7,
-                       :slam_dunks => 2},{},{},{},{}]}
+                       :slam_dunks => 2},
+                          {:player_name => "Jeff Adrien",
+                          :number => 4,
+                          :shoe => 18,
+                          :points => 10,
+                          :rebounds => 1,
+                          :assists => 1,
+                          :steals => 2,
+                          :blocks => 7,
+                          :slam_dunks => 2},{},{},{}]}
 }
 
 
