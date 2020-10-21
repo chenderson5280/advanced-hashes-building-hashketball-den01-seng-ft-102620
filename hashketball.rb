b5dd2a0d2c4f5091d838a1798ce14318245bdb40
Christopher Henderson <chenderson5280@gmail.com>
2020-10-21 16:15:54 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,15 @@
 def game_hash
-{:home => {:team_name => "",
-          :colors => ["",""],
-          :players => [{},{}]
+{:home => {:team_name => "Brooklyn Nets",
+          :colors => ["Black","White"],
+          :players => [{:player_name
+                      :number
+                      :shoe
+                      :points
+                      :rebounds
+                      :assists
+                      :steals
+                      :blocks
+                      :slam_dunks},{}]
         }
 
   :away => {}
