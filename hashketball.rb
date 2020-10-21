2f9812246ce51eeabce5d49e1e83dbfd2bbeedf2
Christopher Henderson <chenderson5280@gmail.com>
2020-10-21 16:37:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -53,17 +53,3 @@ def game_hash
 
 
 end
-
-The :players key points to an Array of Hashes. Each Hash in the Array should contain
-the players' stats. Each stat should be a key that points
-to a value for that stat. The keys should be:'
-
-:player_name
-:number
-:shoe
-:points
-:rebounds
-:assists
-:steals
-:blocks
-:slam_dunks
